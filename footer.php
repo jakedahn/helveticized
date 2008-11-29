4807e33f7a1d3276f413964371442996d323fae8
Jake Dahn <admin@jakedahn.com>
2008-11-29 04:14:48 -0600

    changes

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 
 </div>
+<script src="http://jakedahn.com/mint/?js" type="text/javascript"></script>
 
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-277881-2";
+urchinTracker();
+</script>
 </body>
 </html>
